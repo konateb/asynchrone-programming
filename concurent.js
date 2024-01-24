e6b12b45de57ad642d3abeb9acc30d903c14c413
konateb <111666644+konateb@users.noreply.github.com>
2024-01-23 20:32:01 -0500

    remove timeout from delay

--- concurent.js
+++ concurent.js
@@ -2,32 +2,34 @@
 import { exec, execSync } from "child_process";
 async function runAsyncPromise() {
   const start = Date.now();
-  await Promise.all([delay(1000), delay(2000)]);
+  await Promise.all([delay("sleep 1"), delay("sleep 2")]);
   // Prints about 1000, because the `delay()` calls run in parallel
-  console.log("Time Elapsed:", (Date.now() - start) / 1000, " second(s)");
+  timeElapsed("AsyncPromise:", start);
 }
 
-function delay(timeDelay) {
-  return new Promise((resolve) => setTimeout(resolve, timeDelay));
+function delay(timeSleep) {
+  return new Promise((resolve) => resolve(exec(timeSleep)));
 }
 
 function runSync() {
   const start = Date.now();
   execSync("sleep 1");
   execSync("sleep 2");
-  console.log("Time Elapsed:", (Date.now() - start) / 1000, " second(s)");
+  timeElapsed("Sync:", start);
 }
+
 function runAsync() {
   const start = Date.now();
   exec("sleep 1");
   exec("sleep 2");
-  console.log("Time Elapsed:", (Date.now() - start) / 1000, " second(s)");
+  timeElapsed("Async:", start);
 }
 /////////////////////////////// Testing paralelism
+function timeElapsed(fnName, start) {
+  console.log(fnName, (Date.now() - start) / 1000, " second(s)");
+}
 runAsyncPromise();
-runSync();
-/** 
- * runAsync();
-*/
+runAsync();
+// runSync();
 
 console.log("=============== DONE =================");
--- primeAppMultiThreads.js
+++ primeAppMultiThreads.js
@@ -2,18 +2,19 @@
 import { isMainThread, Worker } from "worker_threads";
 import os from "os";
 // const numWorkers = os.cpus().length;
-const numWorkers = 5;
+const numWorkers = 8;
 
 console.log("CPU Core", numWorkers);
 const LOWER_BOUND = 2; // Adjust the range as needed
-const UPPER_BOUND = 10_000_000;
-const startTime = Date.now();
+const UPPER_BOUND = 100_000_000;
+
 const segmentSize = Math.floor((UPPER_BOUND - LOWER_BOUND) / numWorkers);
 let activeWorkers = numWorkers;
 let totalPrimes = 0;
 let allLastPrimes = [];
 
 if (isMainThread) {
+  const startTime = Date.now();
   for (let i = 0; i < numWorkers; i++) {
     const start = LOWER_BOUND + i * segmentSize;
     const end = i === numWorkers - 1 ? UPPER_BOUND : start + segmentSize;
@@ -25,10 +26,13 @@ if (isMainThread) {
       totalPrimes += count;
       allLastPrimes.push(...lastPrimes);
       if (--activeWorkers === 0) {
+        // get the time elapsed 
+        const timeElapsed = (Date.now() - startTime) / 1000;
         // Specifying US locale:en-US(commas separated)
-        const totalPrimesNumberUS = totalPrimes.toLocaleString("en-US");
+        
         const upperBound = UPPER_BOUND.toLocaleString("en-US");
         const lowerbound = LOWER_BOUND.toLocaleString("en-US");
+        const totalPrimesNumberUS = totalPrimes.toLocaleString("en-US");
         console.log(
           `Total primes between ${lowerbound} and ${upperBound} are ${totalPrimesNumberUS}`
         );
@@ -36,7 +40,7 @@ if (isMainThread) {
           .sort((a, b) => a - b)
           .slice(-10);
         console.log(`Unique last 10 primes: ${uniqueLastPrimes.join(", ")}`);
-        const timeElapsed = (Date.now() - startTime) / 1000;
+        
 
         // Specifying formatting options
         const options = {
