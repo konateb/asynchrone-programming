d0264b46fe809b5a388aa0ca1dbdd28a8f086ce7
konateb <111666644+konateb@users.noreply.github.com>
2024-01-23 20:37:48 -0500

    clean up comment

--- concurent.js
+++ concurent.js
@@ -1,15 +1,5 @@
 //https://masteringjs.io/tutorials/node/sleep
 import { exec, execSync } from "child_process";
-// async function runAsyncPromise() {
-//   const start = Date.now();
-//   await Promise.all([delay("sleep 1"), delay("sleep 2")]);
-//   // Prints about 1000, because the `delay()` calls run in parallel
-//   timeElapsed("AsyncPromise:", start);
-// }
-
-// function delay(timeSleep) {
-//   return new Promise((resolve) => resolve(exec(timeSleep)));
-// }
 
 function runSync() {
   const start = Date.now();
