e252349369fa4ee09e97501f11c4b9a927128388
konateb <111666644+konateb@users.noreply.github.com>
2024-02-06 14:15:49 -0500

    Clean up

--- concurent.js
+++ concurent.js
@@ -1,40 +1,39 @@
 import { exec, execSync } from "child_process";
-function timeElapsed(start) {
+function timeElapsed(method, start) {
   let delay = (Date.now() - start) / 1000;
+  // console.log(method, "time elapsed", Math.floor(delay));
   return delay;
 }
 const printTimeElapsed = () => {
   console.log("Synch TOTAL TIME: ", Math.floor(totalTimeSleeped));
 };
-let totalTimeSleeped = 0;
+let totalTimeSleeped = 0; //Synch total time
+
 function runSync(timeToSleep) {
-  let start1 = Date.now();
+  let start = Date.now();
   execSync(timeToSleep);
-  const timeElapse = Math.floor(timeElapsed(start1));
+  const timeElapse = Math.floor(timeElapsed("Sync:", start));
   totalTimeSleeped += timeElapse;
-  console.log("Sync time elapsed:", timeElapse);
 }
 
 function execAsync(timeToSleep) {
   return new Promise((resolve) => {
     let start = Date.now();
     exec(timeToSleep, () => {
-      resolve(timeElapsed(start));
+      resolve(timeElapsed("Async", start));
     });
   });
 }
 
-const runPromise1 = execAsync("sleep 2");
-const runPromise2 = execAsync("sleep 5");
-const runPromise3 = execAsync("sleep 5");
+const runAsync1 = execAsync("sleep 2");
+const runAsync2 = execAsync("sleep 5");
+const runAsync3 = execAsync("sleep 5");
+Promise.all([runAsync1, runAsync2, runAsync3]).then((values) => {
+  console.log("Async total time elapsed:", Math.floor(Math.max(...values)));
+});
 
-// Promise.all([runPromise1, runPromise2]).then((values) => {
-//   console.log(" total", Math.max(...values));
-// })
-
-runSync("sleep 3");
-runSync("sleep 5");
-runSync("sleep 5");
-
-printTimeElapsed();
+// runSync("sleep 3");
+// runSync("sleep 5");
+// runSync("sleep 5");
+// printTimeElapsed();
 console.log("=============== DONE =================");
